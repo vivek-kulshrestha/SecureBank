b96779756051bf74cd42b129fd80cea223cfb382
vivek-kulshrestha <103198137+vivek-kulshrestha@users.noreply.github.com>
2023-10-05 19:57:58 +0530

    Update main.tf

--- DEV-INFRA/main.tf
+++ DEV-INFRA/main.tf
@@ -1,10 +1,9 @@
 # Create new storage bucket in the US multi-region
 # with standard storage
 
-resource "google_storage_bucket" "static" {
+resource "google_storage_bucket" "static" 
  name          = "in-gcp-apa-con-saadk-sbx-1-bucket-test"
  location      = "US"
  storage_class = "STANDARD"
 
  uniform_bucket_level_access = true
-}
