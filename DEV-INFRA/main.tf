c64cdfe496b8c343657b5eed3eda1fab4a40a214
vivek-kulshrestha <vivek.0485@gmail.com>
2023-10-11 00:04:39 +0530

    resource add

--- DEV-INFRA/main.tf
+++ DEV-INFRA/main.tf
@@ -17,3 +17,26 @@ resource "google_storage_bucket" "bucket" {
   project                     = "analog-provider-401506"
   location                    = "asia-south1"
     }
+
+resource "google_compute_network" "vpc_network" {
+    name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+    name                = "terraform-instance"
+    machine_type        = "f1-micro"
+    tags                 = ["web", "dev"]
+    boot_disk {
+      initialize_params {
+          image = "cos-cloud/cos-stable"
+      }
+    }
+    network_interface {
+        network = google_compute_network.vpc_network.self_link
+        access_config {
+            nat_ip = google_compute_address.vm_static_ip.address
+        }
+    }
+}
+resource "google_compute_address" "vm_static_ip" {
+    name = "terraform-static-ip"
+}
